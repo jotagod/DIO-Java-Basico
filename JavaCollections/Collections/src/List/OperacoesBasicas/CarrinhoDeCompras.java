f7e09b00c9918b568b0d29dbc9ca2376bd8e7df3
João Aurélio <joao_mcd@hotmail.com>
2024-07-10 17:00:59 -0300

    lista de tarefas

--- JavaCollections/Collections/src/List/OperacoesBasicas/CarrinhoDeCompras.java
+++ JavaCollections/Collections/src/List/OperacoesBasicas/CarrinhoDeCompras.java
@@ -1,4 +1,6 @@
-public class App {
+package List.OperacoesBasicas;
+
+public class CarrinhoDeCompras {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
